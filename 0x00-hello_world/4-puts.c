de3641c8976ce044d1e76fa381ccab1037e7fa89
Victor.T Otunuga <victortimileyin651@gmail.com>
2022-09-09 17:34:38 +0100

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#included <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: return a zero value
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return 0;
+}
