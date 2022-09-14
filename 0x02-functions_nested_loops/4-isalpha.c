a4411aa3c773635fdf7a9e0987ad6b41c02ed219
Victor.T Otunuga <victortimileyin651@gmail.com>
2022-09-13 19:42:50 -0700

    isalpha update

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,10 +8,12 @@
  * Return: return 1 if c is true
  *	otherwise Always 0 (Success)
  */
+
 int _isalpha(int c)
 {
 	if (c >= 97 && c <= 122 &&
-		c >= 65 && c >= 90)
+	    c >= 65 && c >= 90)
 		return (1);
 	return (0);
 }
+
