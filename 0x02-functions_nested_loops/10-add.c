2485c7da78175fe71527adf21d05d52a11914161
Victor.T Otunuga <victortimileyin651@gmail.com>
2022-09-14 20:06:33 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ *
+ * @na: integer a
+ *
+ * @nb: integer b
+ *
+ * Description: give two integers, returns sum
+ *
+ * Return: sum of integers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
