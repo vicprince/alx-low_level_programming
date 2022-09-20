a978c3b55ff66af47801840e9fe41228f003a901
Victor.T Otunuga <victortimileyin651@gmail.com>
2022-09-20 12:38:58 -0700

    working with pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+/**
+ * reset_to_98 - takes a pointer and updates;
+ *		and change value to 98.
+ * @n: integer
+ * Return : return 0 Always (Success)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
