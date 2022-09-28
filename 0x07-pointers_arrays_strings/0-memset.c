f123e6c1a9e4ee83d7e1e6ff82be2fde8b9519fc
Victor.T Otunuga <victortimileyin651@gmail.com>
2022-09-27 18:48:45 -0700

    Memory byte

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,16 +2,18 @@
 
 /**
  * _memset - sets memory to array
+ *
  * @s: array to set
  * @b: value to set it as
- * @n: n amount of times
+ * @n: number of bytes
+ *
  * Return: char value of s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for ( i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
 	}
