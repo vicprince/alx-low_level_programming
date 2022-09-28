28dc5a72d1c85025293a1c72cd8657bc96d09e8b
Victor.T Otunuga <victortimileyin651@gmail.com>
2022-09-27 19:32:40 -0700

    string length

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,17 +9,20 @@ unsigned int _strspn(char *s, char *accept)
 {
 	int i;
 	int j;
-	unsigned result = 0;
+	unsigned int result = 0;
 
-	for ( i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for ( j = 0; s[j] != '\0'; j++)
+		for (j = 0; s[j] != '\0'; j++)
 		{
-			result++
-			break;
+			if (s[i] == accept[j])
+			{
+				result++;
+				break;
+			}
 		}
-		if (s[i] != '\0')
+		if (s[j] == '\0')
 			return (result);
 	}
-	return (0);
+	return (result);
 }
