c73708dec29e5abf66318856d836045b390b2035
Victor.T Otunuga <victortimileyin651@gmail.com>
2022-09-27 19:30:23 -0700

    string search

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -12,10 +12,11 @@ char *_strpbrk(char *s, char *accept)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; s[j] != '\0'; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
-				return (s + 1);
+				return (s + i);
 		}
 	}
+	return (0);
 }
