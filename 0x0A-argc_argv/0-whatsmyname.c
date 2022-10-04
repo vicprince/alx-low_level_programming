dccc8dac9c1cc02e1882f96d0fc79b5781d1b6be
Victor.T Otunuga <victortimileyin651@gmail.com>
2022-10-04 06:59:30 -0700

    argc & argv[]

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - prints name
+ * @argc: hold the number of arguments
+ * @argv: array pointer where th arguments are stored
+ *
+ * Return: Always 0 Success
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	int i = 0;
+
+	printf("%s\n", argv[i]);
+	return (0);
+}
