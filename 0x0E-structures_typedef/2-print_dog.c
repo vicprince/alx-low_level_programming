93ff68916f04c2ee752425510e985488b9ad9027
Victor.T Otunuga <victortimileyin651@gmail.com>
2022-10-11 07:40:59 -0700

    C structures

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
+/**
+ * print_dog - print dog
+ *
+ * @d: pointer to struct dog
+ *
+ * Return: nothing
+*/
 void print_dog(struct dog *d)
 {
 	if (d != NULL)
